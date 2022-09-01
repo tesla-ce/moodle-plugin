7ab8627f170d471fb80dee556385a5f8c89610a9
Roger <roger@rcod.net>
2022-09-01 11:19:58 +0200

    update moodle 4 and version to 2022090111170000

--- teslace_plugin/version.php
+++ teslace_plugin/version.php
@@ -18,7 +18,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021111616470000;
+$plugin->version = 2022090111170000;
 $plugin->requires = 2019111800;
 $plugin->cron = 0;
 $plugin->component = 'local_teslace';
